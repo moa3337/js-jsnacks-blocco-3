0eb7c81d1801a91014fcb9a647380240ddcf1837
moa3337 <moa-x@hotmail.com>
2023-01-30 14:05:55 +0100

    final commit Snack 1

--- Snack 1/js/main.js
+++ Snack 1/js/main.js
@@ -1,5 +1,21 @@
 /*
- Ottieni un numero casuale in un range specificato.
- Chiedi all'utente il valore minimo ed il valore
- massimo del range in cui generare il numero.
+ chiedo all'utente un numero minimo,
+ chiedo all'utente un numero massimo,
+ genero un numero compreso tra il valore minimo
+ ed il valore massimo forniti dall'utente
 */
+
+// RICHIESTA PER L'UTENTE DEI NUMERI MAX e MIN
+let numMin = parseInt(prompt("inserisci un numero minimo"));
+let numMax = parseInt(prompt("inserisci un numero massimo"));
+
+// CICLO CON L'USO DEL WHILE
+while (isNaN(numMin) || isNaN(numMax)) {
+    numMin = parseInt(prompt("Error: inserisci un numero minimo"));
+    numMax = parseInt(prompt("Error: inserisci un numero massimomo"));
+}
+ // GENERO UN NUMERO RANDOM TRA IL MIN ED IL MAX
+ let randomNum = Math.floor(Math.random() * (numMax - numMin +1 ) + numMin);
+
+ // STAMPO IL RISULTATO IN CONSOLE
+ console.log(`il tuo numero fortunato è: ${randomNum}`);
