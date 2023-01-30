c5257a3c271b84469a7dfa12b3d52f6efce89412
moa3337 <moa-x@hotmail.com>
2023-01-30 15:16:29 +0100

    final commit Snack 2

--- Snack 2/js/main.js
+++ Snack 2/js/main.js
@@ -4,3 +4,27 @@
  fino a quando la somma degli elementi è minore di 50.
 */
 
+// ARRAY VUOTO
+let empty = [];
+let sum = 0;
+
+// IL CICLO CON WHILE DOVE LA SOMMA DEVE ESSERE MINORE DI 50
+while (sum < 50) {
+
+    // CHIEDO ALL'UTENTE UN NUMERO
+    let number = parseInt(prompt("inserisci un numero"));
+
+    // ALL'ARRAY VUOTO AGGIUNGO IL NUMERO DELL'UTENTE
+    empty.push(number);
+
+    // LA SOMMA INFERIORE A 50 SARA' UGUALE AL NUMERO DELL'UTENTE
+    sum += number;
+}
+
+// DIRE ALL'UTENTE CHE HA SUPERATO 50
+console.log("hai superato: " + 50);
+
+// DIRE ALL'UTENTE I NUMERI CHE HA DIGITATO
+console.log("I tuoi numeri sono: " + empty);
+
+
