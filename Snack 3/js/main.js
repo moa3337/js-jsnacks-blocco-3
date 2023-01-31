6be93a75474f8ce10167717747fed4993d7bf694
moa3337 <moa-x@hotmail.com>
2023-01-31 10:31:21 +0100

    correzione final commit Snack 3

--- Snack 3/js/main.js
+++ Snack 3/js/main.js
@@ -3,3 +3,26 @@
  Genera N array, ognuno formato da 10 numeri casuali da 1 a 100.
  Ogni volta che ne crei uno, stampalo.
 */
+
+// CHIEDEO ALL'UTENTE DI INSERIRE UN NUMERO
+const number = parseInt(prompt("inserisci un numero"));
+
+// CILCLO WHILE 
+let i = 0;
+
+while (i < number) {
+    let array = [];
+
+    // SECONDO CICLO WHILE, PER GENERARRE I NUMERI RANDOM
+    let box = 0;
+
+    while (box < 10) {
+        array.push(Math.floor(Math.random() * 100 + 1));
+        box++;
+    }
+    
+    // STANPA IN CONSOLE IL NUMERO DI ARRAY CON DENTRO I NUMERI DA 1 A 100
+    console.log(`array ${i + 1}:` + array);
+    
+    i++
+}
