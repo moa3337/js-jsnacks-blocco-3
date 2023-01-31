e0f9fa39b29bfd0d5e570a2fdb416718eb164acb
moa3337 <moa-x@hotmail.com>
2023-01-31 09:46:22 +0100

    correzione final commit Snack 1

--- Snack 1/js/main.js
+++ Snack 1/js/main.js
@@ -1,13 +1,12 @@
 /*
- chiedo all'utente un numero minimo,
- chiedo all'utente un numero massimo,
- genero un numero compreso tra il valore minimo
- ed il valore massimo forniti dall'utente
+ chiedo all'utente un numero minimo e un numero massimo,
+ genero un numero compreso tra il valore minimo ed il
+ valore massimo forniti dall'utente.
 */
 
 // RICHIESTA PER L'UTENTE DEI NUMERI MAX e MIN
-let numMin = parseInt(prompt("inserisci un numero minimo"));
-let numMax = parseInt(prompt("inserisci un numero massimo"));
+const numMin = parseInt(prompt("inserisci un numero minimo"));
+const numMax = parseInt(prompt("inserisci un numero massimo"));
 
 // CICLO CON L'USO DEL WHILE
 while (isNaN(numMin) || isNaN(numMax)) {
